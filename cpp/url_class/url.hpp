d31b466bac056e495b00b13172adb899c7e110ea
Mike Ulicny <michaeaulicny@gmail.com>
2019-12-14 17:09:59 -0500

    changed to better function formatting for class

--- cpp/url_class/url.hpp
+++ cpp/url_class/url.hpp
@@ -10,33 +10,9 @@ class Url {
         Url(const Url&) = delete;
         Url &operator=(const Url&) = delete;
 
-        
+        void set(CURLUPart part, const char *content);
+        const std::string get(CURLUPart part);
 
-        // set member functions 
-        void set_url(const std::string &new_url);
-        void set_scheme(const std::string &new_scheme);
-        void set_user(const std::string &new_user);
-        void set_password(const std::string &new_password);
-        void set_options(const std::string &new_options);
-        void set_host(const std::string &new_host);
-        void set_zoneid(const std::string &new_zoneid);
-        void set_port(const std::string &new_port);
-        void set_path(const std::string &new_path);
-        void set_query(const std::string &new_query);
-        void set_fragment(const std::string &new_fragment);
-
-        // get member functions
-        const std::string get_url();
-        const std::string get_scheme();
-        const std::string get_user();
-        const std::string get_password();
-        const std::string get_options();
-        const std::string get_host();
-        const std::string get_zoneid();
-        const std::string get_port();
-        const std::string get_path();
-        const std::string get_query();
-        const std::string get_fragment();
     private:
         CURLU *m_url;
         CURLUcode m_curlcode;
--- cpp/url_class/url_class_test.cpp
+++ cpp/url_class/url_class_test.cpp
@@ -8,29 +8,17 @@ int main()
 {
     // create empty url
     Url test_url; 
-    test_url.set_url("https://google.com/pathA/pathB?query1=abc&query2=xyz");
+    test_url.set(CURLUPART_URL, "https://google.com/pathA/pathB?query1=abc&query2=xyz");
 
     std::string temp;
-    temp = test_url.get_url();
-    std::cout << "get_url: " << temp << "\n";
+    temp = test_url.get(CURLUPART_URL);
+    std::cout << "Url: " << temp << "\n";
 
-    temp = test_url.get_scheme();
-    std::cout << "get_scheme: " << temp << "\n";
+    temp = test_url.get(CURLUPART_SCHEME);
+    std::cout << "Scheme: " << temp << "\n";
 
-    temp = test_url.get_host();
-    std::cout << "get_host: " << temp << "\n";
+    temp = test_url.get(CURLUPART_QUERY);
+    std::cout << "Query: " << temp << "\n";
 
-    //temp = test_url.get_port();
-    //std::cout << "get_port: " << temp << "\n";
-
-    temp = test_url.get_path();
-    std::cout << "get_path: " << temp << "\n";
-
-    temp = test_url.get_query();
-    std::cout << "get_query: " << temp << "\n";
-
-    //temp = test_url.get_fragment();
-    //std::cout << "get_fragment: " << temp << "\n";
-    
     return 0;
 }
